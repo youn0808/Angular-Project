922d3ba2025483e6224fed634ed19470bb15b795
Seung <hhwannn2@gmail.com>
2024-06-01 03:08:53 -0500

    Add User Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './header/header.components'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/header/header.components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.components.css',
+  })
+
+
+export class HeaderComponent{}
